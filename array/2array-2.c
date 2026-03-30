8849664d8ccfc7aaafa5d4b39f4d16de889a450b
kimjihu <kimjihuuu@gmail.com>
2026-03-30 15:21:48 +0900

    2차수정

--- array/2array-2.c
+++ array/2array-2.c
@@ -3,15 +3,13 @@
 int main(void)
 {
     int i;
-    char menu[5][10] = { //5행 10열 (0~4행, 0~9열)
+    char menu[3][10] = { //3행 10열 (0~2행, 0~9열)
         "init", //1행식 초기값 설정
         "open",
-        "close",
-        "read",
-        "write"
+        "close"
     };
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 3; i++)
         printf("%d 번째 메뉴: %s \n", i+1, menu[i]); //열을 생략한 것            행을 의미
 
     return 0;
