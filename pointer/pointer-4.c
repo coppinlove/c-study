b8584e411c5a98ccb36ad9ea6c7befdfdcf10dc1
kimjihu <kimjihuuu@gmail.com>
2026-04-14 15:22:58 +0900

    포인터

---
+++ pointer/pointer-4.c
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int count_even(int ar[10]);
+void print_cnt(int res);
+
+int main(void)
+{
+	int ar[10];
+	printf("정수 10개 입력: ");
+	for (int a = 0; a < 10; a++)
+	{
+		scanf("%d", &ar[a]);
+	}
+	int res = count_even(ar);
+	print_cnt(res);
+	/*printf("짝수의 개수는 %d개 입니다", res);*/
+	
+
+
+	return 0;
+}
+
+
+int count_even(int ar[10])
+{
+
+	int cnt = 0;
+
+	for (int a = 0; a < 10; a++)
+	{
+		ar[2] = 4; //여기서 바꿔도 main함수에서도 바뀜 배열 자체가 주소임 참조함
+		if (ar[a] % 2 == 0)
+		{
+		cnt += 1;
+		}
+	}
+
+	return cnt;
+}
+
+void print_cnt(int res)
+{
+	printf("짝수의 개수는 %d개 입니다", res);
+}
---
+++ pointer/pointer-5.c
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int adddddddddddddddd(int a, int b);
+void suuuuuuuuuuuuuub(int a, int b);
+void arrrrrrprint(int c[5]);
+
+
+int main(void)
+{
+	int a = 10, b = 20, c[5] = { 1,2,3,4,5 };
+	int sum = adddddddddddddddd(a, b);
+	printf("덧셈 결과: %d\n", sum);
+	suuuuuuuuuuuuuub(a, b);
+
+	arrrrrrprint(c);
+	return 0;
+}
+
+int adddddddddddddddd(int a, int b)
+{
+	int sum = a + b;
+
+	return sum;
+}
+
+void suuuuuuuuuuuuuub(int a, int b)
+{
+	int sub = a - b;
+	printf("뺄셈 결과: %d\n", sub);
+}
+
+void arrrrrrprint(int c[5])
+{
+	for (int a = 0; a < 5; a++)
+	{
+		printf("%d\n", c[a]);
+	}
+}
